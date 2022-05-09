3efe967901d3dbb534c88d2b395284c15ffe830c
Matt Travi <programmer@travi.org>
2022-05-08 21:12:57 -0500

    feat: defined the filename pattern for test files

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,6 +1,7 @@
 export default function () {
   return {
     devDependencies: ['vitest'],
-    scripts: {'test:unit:base': 'DEBUG=any vitest run'}
+    scripts: {'test:unit:base': 'DEBUG=any vitest run'},
+    testFilenamePattern: 'src/**/*.test.js'
   };
 }
--- src/scaffolder.test.js
+++ src/scaffolder.test.js
@@ -4,9 +4,10 @@ import scaffold from './scaffolder';
 
 describe('scaffolder', () => {
   it('that core details are defined', async () => {
-    const {devDependencies, scripts} = await scaffold();
+    const {devDependencies, scripts, testFilenamePattern} = await scaffold();
 
     assert.deepEqual(scripts, {'test:unit:base': 'DEBUG=any vitest run'});
     assert.deepEqual(devDependencies, ['vitest']);
+    assert.equal(testFilenamePattern, 'src/**/*.test.js');
   });
 });
