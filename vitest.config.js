766bbd8348baa217e8532a8f7267f2622b3979c9
Matt Travi <programmer@travi.org>
2026-03-14 11:03:48 -0500

    test(coverage): switch from c8 to the vitest built-in coverage

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,9 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     restoreMocks: true,
-    mockReset: true
+    mockReset: true,
+    coverage: {
+      provider: 'v8'
+    }
   }
 });
