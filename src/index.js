77b4aa6479cd721f32353de63a96881ab031c1f3
Matt Travi <programmer@travi.org>
2026-03-18 18:28:38 -0500

    refactor(config): move the base tester under the config directory
    since it is checking for config file presence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export {default as scaffold} from './scaffolder.js';
-export {default as test} from './tester.js';
+export {test} from './config/index.js';
 export {default as lift} from './lifter.js';
